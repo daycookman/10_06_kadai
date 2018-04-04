20c336d160c47ad3c72879f0c5f74dfc7bb9a37b
Kazuki Daikoku <daycookman@gmail.com>
2018-04-05 01:09:42 +0900

    再提出

--- Slot/src/com/company/App.java
+++ Slot/src/com/company/App.java
@@ -1,19 +1,19 @@
 package com.company;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.util.Duration;
+import javafx.scene.control.Label;
+import javafx.scene.Scene;
+
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Collections;
+
 
 public class App extends Application {
     public static void main(String[] args) {
@@ -21,47 +21,99 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception{
-//        Label SlotLabel = new Label("1");
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Slot Machine");
 
 //        表示する数字の配列
-        String[] number_array = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+//        String[] number_array = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+        //数字を表示するHBoxを作成
+        HBox slot_box = new HBox(20d);
+        slot_box.setAlignment(Pos.CENTER);
 
         ArrayList<SlotLabel> label_array = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
-            label_array.add(new SlotLabel());
+            label_array.add(new SlotLabel("1"));
         }
 
-        //数字を表示するHBoxを作成
-        HBox numBox = new HBox(20d);
-        numBox.setPadding(new Insets(10,10,10,10));
-        numBox.setAlignment(Pos.CENTER);
+        slot_box.getChildren().addAll(label_array);
 
-        numBox.getChildren().addAll(label_array);
-
+        //写経部分
         //ボタンの作成
-        Button start_button = new Button("START");
-        start_button.setPrefSize(100,50);
-        Button stop_button = new Button("STOP");
-        stop_button.setPrefSize(100,50);
+        HBox buttonBox = new HBox(10d);
+        buttonBox.setAlignment(Pos.CENTER);
+        Button startButton = new Button("START");
+        Button stopButton = new Button("STOP");
+        buttonBox.getChildren().add(startButton);
+        buttonBox.getChildren().add(stopButton);
+        startButton.setPrefSize(100,50);
+        stopButton.setPrefSize(100,50);
 
-        //STARTボタンを押した時の処理
-        start_button.setOnAction(event -> {
-            Random rand =new Random();
-            Timeline timer = new Timeline(new KeyFrame(Duration.millis(50), e ->{
-                int randomNumber = rand.nextInt(number_array.length);
-                //うまくいかない箇所
-                start_button.setText(number_array[randomNumber]);
-            }));
+        VBox all_box = new VBox(20d);
+        all_box.setAlignment(Pos.CENTER);
+        all_box.getChildren().add(slot_box);
+        all_box.getChildren().add(buttonBox);
 
-            timer.setCycleCount(Timeline.INDEFINITE);
-            timer.play();
-
-            //STOPボタンを押した時の処理
-            stop_button.setOnAction(e -> {
-                timer.stop();
-            });
+        // StartボタンのAction設定
+        startButton.setOnAction(event -> {
+            for (int i = 0; i < label_array.size(); i++) {
+                label_array.get(i).setSlotStarted();
+            }
         });
+
+        // StopボタンのAction設定
+        stopButton.setOnAction(event -> {
+            for(int i = 0; i < label_array.size() ;i++){
+                label_array.get(i).stopSlot();
+            }
+
+            ArrayList<String> res = new ArrayList<>();
+            for(int i = 0; i < label_array.size() ;i++){
+                res.add(label_array.get(i).getText());
+            }
+
+            if (Collections.frequency(res, res.get(0)) == res.size()) {
+                // 新しいウインドウを生成
+                Stage newStage = new Stage();
+
+                // モーダルウインドウに設定
+                newStage.initModality(Modality.APPLICATION_MODAL);
+                // オーナーを設定
+                newStage.initOwner(primaryStage);
+
+                // 新しいウインドウ内に配置するコンテンツを生成
+                HBox hbox = new HBox();
+                Label label = new Label("Congratulations！");
+                label.setFont(new Font(20d));
+                hbox.getChildren().add(label);
+
+                newStage.setScene(new Scene(hbox));
+
+                // 新しいウインドウを表示
+                newStage.show();
+
+            }
+        });
+
+
+          //自分で書いたコード
+//        //STARTボタンを押した時の処理
+//        start_button.setOnAction(event -> {
+//            Random rand =new Random();
+//            Timeline timer = new Timeline(new KeyFrame(Duration.millis(50), e ->{
+//                int randomNumber = rand.nextInt(number_array.length);
+//                //うまくいかない箇所
+//                start_button.setText(number_array[randomNumber]);
+//            }));
+//
+//            timer.setCycleCount(Timeline.INDEFINITE);
+//            timer.play();
+//
+//            //STOPボタンを押した時の処理
+//            stop_button.setOnAction(e -> {
+//                timer.stop();
+//            });
+//        });
 
 
 
@@ -89,22 +141,21 @@ public class App extends Application {
 //            }
 //        });
 
+//            //ボタンを表示するHBoxの作成
+//        HBox hBox = new HBox(20);
+//        hBox.setAlignment(Pos.CENTER);
+//        hBox.getChildren().add(start_button);
+//        hBox.getChildren().add(stop_button);
+//
+//        //borderPaneに追加
+//        BorderPane borderPane = new BorderPane();
+//        borderPane.setPadding(new Insets(30,10,10,10));
+//        borderPane.setTop(numBox);
+//        borderPane.setCenter(hBox);
 
-            //ボタンを表示するHBoxの作成
-        HBox hBox = new HBox(20);
-        hBox.setAlignment(Pos.CENTER);
-        hBox.getChildren().add(start_button);
-        hBox.getChildren().add(stop_button);
-
-        //borderPaneに追加
-        BorderPane borderPane = new BorderPane();
-        borderPane.setPadding(new Insets(30,10,10,10));
-        borderPane.setTop(numBox);
-        borderPane.setCenter(hBox);
-
-        Scene scene = new Scene(borderPane,400,200);
-        stage.setScene(scene);
-        stage.show();
+        Scene scene = new Scene(all_box,400,200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
     }
 }
--- Slot/src/com/company/SlotLabel.java
+++ Slot/src/com/company/SlotLabel.java
@@ -1,28 +1,58 @@
 package com.company;
 
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import java.util.Random;
+import javafx.util.Duration;
 
 public class SlotLabel extends Label {
     // アニメーションを行うためのTimeLineオブジェクト
-//    private Timeline timer;
+    private Timeline timer;
     // スロットが回っている状態かを管理するフィールド
-//    private boolean isSlotStarted;
+    private boolean isSlotStarted;
 
-    SlotLabel(){
-        //この下にthis.~という形で数字の見た目・揃える位置を記載しましょう
-        super();
-        this.setText("1");
-        this.setPrefSize(50,50);
-
+    SlotLabel(String text){
+        super(text);
+        isSlotStarted = false;
+        // この下にthis.~という形で数字の見た目・揃える位置を記載しましょう
+        this.setPrefSize(100,100);
+        this.setAlignment(Pos.CENTER);
+        this.setBorder(new Border(new BorderStroke(Color.BLACK,
+                BorderStrokeStyle.SOLID, new CornerRadii(5), BorderWidths.DEFAULT)));
     }
 
     public void setSlotStarted(){
         // この中に各数字のスロットが回り始めた時の処理を記載しましょう
+        if (isSlotStarted) {
+            return;
+        }
+        // スロットが回っているのでisSlotStartedをtrueに変える
+        isSlotStarted = true;
+
+        Random rand = new Random();
+
+        timer = new Timeline(new KeyFrame(Duration.millis(100), event -> {
+            int randomNumber = rand.nextInt(9) + 1;
+            this.setText(String.valueOf(randomNumber));
+        }));
+
+        // アニメーションをスタート
+        timer.setCycleCount(Timeline.INDEFINITE);
+        timer.play();
+
     }
 
     public void stopSlot() {
-        // この中に各数字のスロットが止まった時の処理を記載しましょう
-    }
+            // この中に各数字のスロットが止まった時の処理を記載しましょう
 
-}
+            if (isSlotStarted) {
+                timer.stop();
+                isSlotStarted = false;
+            }
+
+        }
+    }
